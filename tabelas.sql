e89fad329b714ae941c7a0fcae7791b6509cb1b6
Paulo Henrique <114195803+paulopaes216@users.noreply.github.com>
2025-04-09 19:27:16 -0300

    Update tabelas.sql

--- tabelas.sql
+++ tabelas.sql
@@ -2,49 +2,107 @@ CREATE DATABASE IF NOT EXISTS clinica_odontologica;
 
 USE clinica_odontologica;
 
-CREATE TABLE IF NOT EXISTS Pacientes (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    cpf VARCHAR(11) UNIQUE NOT NULL,
-    data_nascimento DATE NOT NULL,
-    telefone VARCHAR(20),
-    email VARCHAR(100) UNIQUE,
-    endereco TEXT,
-    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+CREATE TABLE IF NOT EXISTS public."Pacientes"
+(
+    id integer NOT NULL,
+    nome character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    cpf character varying(11) COLLATE pg_catalog."default" NOT NULL,
+    data_nascimento date NOT NULL,
+    telefone integer NOT NULL,
+    email character varying(100) COLLATE pg_catalog."default",
+    data_cadastro timestamp with time zone,
+    endereco character varying(150) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT id_paciente PRIMARY KEY (id)
+)
 
-CREATE TABLE IF NOT EXISTS Dentistas (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    cro VARCHAR(10) UNIQUE NOT NULL,
-    especialidade VARCHAR(50),
-    telefone VARCHAR(20),
-    email VARCHAR(100) UNIQUE
-);
+TABLESPACE pg_default;
 
-CREATE TABLE IF NOT EXISTS Consultas (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    paciente_id INT,
-    dentista_id INT,
-    data_consulta DATETIME NOT NULL,
-    tipo_consulta ENUM('Check-up', 'Urgência', 'Tratamento') NOT NULL,
-    status ENUM('Agendada', 'Realizada', 'Cancelada') DEFAULT 'Agendada',
-    valor DECIMAL(10, 2) NOT NULL,
-    FOREIGN KEY (paciente_id) REFERENCES Pacientes(id) ON DELETE CASCADE,
-    FOREIGN KEY (dentista_id) REFERENCES Dentistas(id) ON DELETE SET NULL
-);
+ALTER TABLE IF EXISTS public."Pacientes"
+    OWNER to postgres;
 
-CREATE TABLE IF NOT EXISTS Tratamentos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    descricao TEXT,
-    preco DECIMAL(10, 2) NOT NULL
-);
+REVOKE ALL ON TABLE public."Pacientes" FROM "Recepcao";
 
-CREATE TABLE IF NOT EXISTS Consulta_Tratamento (
-    consulta_id INT,
-    tratamento_id INT,
-    PRIMARY KEY (consulta_id, tratamento_id),
-    FOREIGN KEY (consulta_id) REFERENCES Consultas(id) ON DELETE CASCADE,
-    FOREIGN KEY (tratamento_id) REFERENCES Tratamentos(id) ON DELETE CASCADE
-);
+GRANT ALL ON TABLE public."Pacientes" TO "Administrador";
+
+GRANT SELECT, INSERT ON TABLE public."Pacientes" TO "Recepcao";
+
+CREATE TABLE IF NOT EXISTS public."Dentistas"
+(
+    id integer NOT NULL DEFAULT nextval('"Dentistas_id_seq"'::regclass),
+    nome character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    cro integer NOT NULL,
+    especialidade character varying(50) COLLATE pg_catalog."default",
+    telefone character varying(12) COLLATE pg_catalog."default",
+    email character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT id_dentista PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public."Dentistas"
+    OWNER to postgres;
+
+REVOKE ALL ON TABLE public."Dentistas" FROM "Recepcao";
+
+GRANT ALL ON TABLE public."Dentistas" TO "Administrador";
+
+GRANT SELECT, INSERT ON TABLE public."Dentistas" TO "Recepcao";
+
+CREATE TYPE public.status_consulta AS ENUM
+    ('Agendada', 'Realizada', 'Cancelada');
+CREATE TYPE public.tipo_consulta AS ENUM
+    ('Check-up', 'Urgência', 'Tratamento');
+
+CREATE TABLE IF NOT EXISTS public."Consultas"
+(
+    id integer NOT NULL DEFAULT nextval('"Consultas_id_seq"'::regclass),
+    paciente_id integer,
+    dentista_id integer,
+    data_consulta date,
+    tipo_consulta character varying(20) COLLATE pg_catalog."default" NOT NULL,
+    status character varying(20) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT "Consultas_pkey" PRIMARY KEY (id),
+    CONSTRAINT dentista_id FOREIGN KEY (dentista_id)
+        REFERENCES public."Dentistas" (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT paciente_id FOREIGN KEY (paciente_id)
+        REFERENCES public."Pacientes" (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+CREATE TABLE IF NOT EXISTS public."Tratamentos"
+(
+    id integer NOT NULL DEFAULT nextval('"Tratamentos_id_seq"'::regclass),
+    nome character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    descricao character(1) COLLATE pg_catalog."default",
+    preco real,
+    CONSTRAINT id_tratamento PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public."Tratamentos"
+    OWNER to postgres;
+
+REVOKE ALL ON TABLE public."Tratamentos" FROM "Recepcao";
+
+GRANT ALL ON TABLE public."Tratamentos" TO "Administrador";
+
+GRANT SELECT, INSERT ON TABLE public."Tratamentos" TO "Recepcao";
+
+
+CREATE TABLE IF NOT EXISTS public."Consulta_Tratamento"
+(
+    consulta_id integer NOT NULL,
+    tratamento_id integer NOT NULL,
+    CONSTRAINT "Consulta_Tratamento_pkey" PRIMARY KEY (consulta_id, tratamento_id),
+    CONSTRAINT "Consulta_id" FOREIGN KEY (consulta_id)
+        REFERENCES public."Consultas" (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT "Tratamento_id" FOREIGN KEY (tratamento_id)
+        REFERENCES public."Tratamentos" (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
